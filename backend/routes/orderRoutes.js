d33273da9fa0440847a1737858942328dc360842
tarin18-10 <rabeyanoortarin2020@gmail.com>
2025-05-04 15:36:08 +1000

    Added GET routes for fetching all orders and order by ID

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
+// Get all orders
+router.get('/', orderController.getAllOrders);
+
+// Get order by ID
+router.get('/:id', orderController.getOrderById);
