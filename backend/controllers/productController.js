be3968a902173ea14ea4aff2d56f7db9a3b137d5
komol21 <komolpaul86@gmail.com>
2025-05-05 11:31:38 +0600

    Create productController.js
    create product control table

---
+++ backend/controllers/productController.js
@@ -0,0 +1,20 @@
+const db = require('../config/db');
+
+exports.getProducts = async (req, res) => {
+  try {
+    const [products] = await db.query(`
+      SELECT 
+        p.*, 
+        b.production_Date, b.expiration_Date, b.total_Weight, b.batch_Status,
+        pu.facility_Name AS processing_facility,
+        w.address AS warehouse_location
+      FROM MeatProduct p
+      JOIN MeatBatch b ON p.batch_ID = b.batch_ID
+      JOIN ProcessingUnit pu ON b.unit_ID = pu.unit_ID
+      LEFT JOIN Warehouse w ON b.warehouse_ID = w.warehouse_ID
+    `);
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
